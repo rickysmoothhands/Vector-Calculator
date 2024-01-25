137de66138b99832e6c1512c5fef1521b8c35449
Derek Herrera-Sturm <143175468+DerekHerreraSturm@users.noreply.github.com>
2024-01-25 07:33:46 -0800

    Update Vector.cs

--- Vector Calculator/Vector.cs
+++ Vector Calculator/Vector.cs
@@ -14,85 +14,79 @@ namespace Vector_Calculator
 
         public Vector(float x, float y, float z)
         {
-            // ADD CONSTRUCTOR CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
+            this.x = x;
+            this.y = y;
+            this.z = z;
         }
 
         public override string ToString()
         {
-            // ADD TOSTRING CODE HERE, THEN REMOVE BELOW LINE
+            return $"<{x}, {y}, {z}>";
+        }
+
+        public float GetMagnitude()
+        {
+            // ADD CODE HERE, THEN REMOVE BELOW LINE
+            throw new NotImplementedException();
+        }
+
+        public float GetDirection()
+        {
+            // ADD CODE HERE, THEN REMOVE BELOW LINE
             throw new NotImplementedException();
         }
 
         public static Vector Add(Vector v1, Vector v2)
         {
-            // ADD ADDITION CODE HERE, THEN REMOVE BELOW LINE
+            // ADD CODE HERE, THEN REMOVE BELOW LINE
             throw new NotImplementedException();
         }
 
         public static Vector Negate(Vector v)
         {
-            // ADD NEGATATION CODE HERE, THEN REMOVE BELOW LINE
+            // ADD CODE HERE, THEN REMOVE BELOW LINE
             throw new NotImplementedException();
         }
 
         public static Vector Subtract(Vector v1, Vector v2)
         {
-            // ADD SUBTRACTION CODE HERE, THEN REMOVE BELOW LINE
+            // ADD CODE HERE, THEN REMOVE BELOW LINE
             throw new NotImplementedException();
         }
 
-        public static Vector Scale(Vector v, float scalar)
+        public static Vector Scale(Vector v)
         {
-            // ADD SCALING CODE HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
-        }
-
-        public static float GetMagnitude(Vector v)
-        {
-            // ADD MAGNITUDE CODE HERE, THEN REMOVE BELOW LINE
+            // ADD CODE HERE, THEN REMOVE BELOW LINE
             throw new NotImplementedException();
         }
 
         public static Vector Normalize(Vector v)
         {
-            // ADD NORMALIZATION CODE HERE, THEN REMOVE BELOW LINE
+            // ADD CODE HERE, THEN REMOVE BELOW LINE
             throw new NotImplementedException();
         }
 
         public static float DotProduct(Vector v1, Vector v2)
         {
-            // ADD DOT PRODUCT CODE HERE, THEN REMOVE BELOW LINE
+            // ADD CODE HERE, THEN REMOVE BELOW LINE
             throw new NotImplementedException();
         }
 
         public static Vector CrossProduct(Vector v1, Vector v2)
         {
-            // ADD CROSS PRODUCT CODE HERE, THEN REMOVE BELOW LINE
+            // ADD CODE HERE, THEN REMOVE BELOW LINE
             throw new NotImplementedException();
         }
 
-        public static float AngleBetween(Vector v1, Vector v2)
+        public static Vector AngleBetween(Vector v1, Vector v2)
         {
-            // ADD ANGLE CODE HERE, THEN REMOVE BELOW LINE
+            // ADD CODE HERE, THEN REMOVE BELOW LINE
             throw new NotImplementedException();
         }
 
-        public static Vector Project(Vector v1, Vector v2)
+        public static Vector ProjectOnto(Vector v1, Vector v2)
         {
-            // ADD PROJECTION HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
-        }
-
-        public static Vector Reflect(Vector v1, Vector v2)
-        {
-            // ADD REFLECTION HERE, THEN REMOVE BELOW LINE
-            throw new NotImplementedException();
-        }
-
-        public bool Equals(Vector obj)
-        {
-            // ADD EQUALS HERE, THEN REMOVE BELOW LINE
+            // ADD CODE HERE, THEN REMOVE BELOW LINE
             throw new NotImplementedException();
         }
     }
